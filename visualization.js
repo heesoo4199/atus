3e3d813e138320cec6654e4629864e3bd063c20a
asherdale <asher@dales.org>
2019-04-23 22:48:56 -0500

    Add flare

--- visualization.js
+++ visualization.js
@@ -42,11 +42,15 @@ var visualize = function(data) {
     svg.append("text")
         .attr("text-anchor", "middle")
         .attr("transform", "translate("+ (-3 * config.margin.left / 4) +","+(config.height/2)+")rotate(-90)")
+        .style("font-size", "16px")
+        .style("font-style", "italic")
         .text("Percent of Population");
 
     svg.append("text")
         .attr("text-anchor", "middle")
         .attr("transform", "translate("+ (config.width/2) +","+(config.height + config.margin.bottom / 2)+")")
+        .style("font-size", "16px")
+        .style("font-style", "italic")
         .text("Time of Day");
 
     const xScale = d3.scaleLinear()
@@ -104,18 +108,18 @@ var visualize = function(data) {
 
     for (let i = 0; i < config.numIncomeGroups; i++) {
         svg.append("path")
-                .data([lineData[i]])
-                .attr("class", "line")
-                .attr("d", line)
-                .style("fill", "none")
-                .style("stroke", colors[i])
-                .style("stroke-width", "2px")
-                .on("mouseenter", () => {
-                    hideAreas();
+            .data([lineData[i]])
+            .attr("class", "line")
+            .attr("d", line)
+            .style("fill", "none")
+            .style("stroke", colors[i])
+            .style("stroke-width", "2px")
+            .on("mouseenter", () => {
+                hideAreas();
 
-                    d3.select(".area-" + i)
-                        .style("visibility", "visible");
-                });
+                d3.select(".area-" + i).style("visibility", "visible");
+            })
+            .call(transition);
     }
 };
 
@@ -124,4 +128,16 @@ function hideAreas() {
         d3.select(".area-" + i)
             .style("visibility", "hidden");
     }
+}
+
+function transition(path) {
+    path.transition()
+        .duration(1000)
+        .attrTween("stroke-dasharray", tweenDash);
+}
+
+function tweenDash() {
+    const l = this.getTotalLength();
+    const i = d3.interpolateString("0," + l, l + "," + l);
+    return t => i(t);
 }
